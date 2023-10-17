e88f827561bf660dab03eb0267aa57896964996c
Diksha Wadnerkar <diksha.wadnerkar@zensar.com>
2023-10-17 12:01:08 +0530

    initial commit

---
+++ src/main/java/com/example/testapp/controller/TestController.java
@@ -0,0 +1,14 @@
+package com.example.testapp.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+	@GetMapping("/hello")
+	public String hello() {
+		
+		return "Hello!!!";
+	}
+}
