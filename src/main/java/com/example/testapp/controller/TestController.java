ed8f233f68a049d50b0cb6b2c2417b40e6e38d2f
Diksha Wadnerkar <diksha.wadnerkar@zensar.com>
2023-10-17 12:53:30 +0530

    added bye

--- src/main/java/com/example/testapp/controller/TestController.java
+++ src/main/java/com/example/testapp/controller/TestController.java
@@ -1,6 +1,7 @@
 package com.example.testapp.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,7 +13,7 @@ public class TestController {
 		return "Hello!!!";
 	}
 	
-	@GetMapping("/bye")
+	@RequestMapping("/bye")
 	public String bye() {
 		
 		return "Bye!!";
